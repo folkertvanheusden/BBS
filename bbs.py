6a910d88574b17d5b4f4e2ae46dceb140568c423
Folkert van Heusden <folkert@vanheusden.com>
2025-08-02 22:14:44 +0100

    fixes

--- bbs.py
+++ bbs.py
@@ -3,11 +3,14 @@
 import ax25.socket
 import os
 import select
+import socket
 import subprocess
+import sys
 import threading
 
+
 def send(s, str_):
-    s.send(str_.encode('ascii'))
+    s.send(str_.encode('utf-8'))
 
 
 def redirect_prog(cmd, socket):
@@ -17,7 +20,7 @@ def redirect_prog(cmd, socket):
             send(socket, line + '\n')
 
     except Exception as e:
-        send(socket, f'Exception {e} while performing command')
+        send(socket, f'Exception {e} while performing command, line number: {e.__traceback__.tb_lineno}\n')
 
 
 def redirect_sockets(to_socket, socket):
@@ -34,18 +37,19 @@ def redirect_sockets(to_socket, socket):
 
             for e in rc:
                 if e[0] == socket:
-                    msg = socket.recv()
+                    msg = socket.recv(256)
                     send(to_socket, msg.decode('ascii') + '\n')
 
                 elif e[0] == to_socket:
-                    msg = to_socket.recv()
+                    msg = to_socket.recv(256)
                     send(socket, msg.decode('ascii') + '\n')
 
         to_socket.close()
         send(socket, 'Disconnected\n')
 
     except Exception as e:
-        send(socket, f'Exception {e} while performing axcall')
+        send(socket, f'Exception {e} while connecting sockets, line number: {e.__traceback__.tb_lineno}\n')
+
 
 def redirect_axcall(callsign, socket):
     try:
@@ -59,7 +63,8 @@ def redirect_axcall(callsign, socket):
         redirect_sockets(to_socket, socket)
 
     except Exception as e:
-        send(socket, f'Exception {e} while performing axcall')
+        send(socket, f'Exception {e} while performing axcall, line number: {e.__traceback__.tb_lineno}\n')
+
 
 def client_handler(s, call):
     h_for_help = True
@@ -70,11 +75,11 @@ def client_handler(s, call):
                 send(s, 'Enter "h" (+ enter) for help\n')
                 h_for_help = False
 
-            cmd = s.recv()
+            cmd = s.recv(256)
             if cmd == None:
                 break
 
-            cmd = cmd.rstrip('\n').rstrip('\r').strip()
+            cmd = cmd.decode('ascii').rstrip('\n').rstrip('\r').strip()
             parts = cmd.split()
 
             if parts[0] == 'h':
@@ -97,15 +102,22 @@ def client_handler(s, call):
                 h_for_help = True
 
     except Exception as e:
-        send(s, f'Internal error: {e}')
+        send(s, f'Internal error: {e}, line number: {e.__traceback__.tb_lineno}\n')
 
-    send(s, 'Bye bye')
+    send(s, 'Bye bye\n\n')
 
     s.close()
 
 
-server = ax25.socket.Socket()
-server.bind('PI1GDA-1')
+if 'tcp' in sys.argv:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(('0.0.0.0', 2300))
+
+else:
+    server = ax25.socket.Socket()
+    server.bind('PI1GDA-1')
+
 server.listen(128)
 
 while True:
